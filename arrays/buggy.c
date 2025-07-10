b76442bee700c5a968ad0b69290498f28e131f32
Silviu <silviu_miron_90@outlook.com>
2025-07-10 10:24:48 +0100

    Create scores.c file

--- arrays/buggy.c
+++ arrays/buggy.c
@@ -1,12 +1,20 @@
 
 #include <stdio.h>
-
+void print_column(int height);
 int main(void)
 
 {
-    for (int i = 0; i < 3; i++)
-    {
+    int h;
+    printf("Enter the hight stars: ");
+    scanf("%d", &h);
+    print_column(h);
+}
 
+void print_column(int height)
+{
+    for (int i = 0; i < height; i++)
+
+    {
         printf("#\n");
     }
 }
